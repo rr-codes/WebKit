a0bd20aa040dba91a4f6782ae72729e1755940b1
Ruthvik Konda <rkonda2@apple.com>
2026-04-08 11:54:28 -0700

    Use protect() instead of Ref { } in Source/WebKit/Shared/WebGPU https://bugs.webkit.org/show_bug.cgi?id=311679 rdar://174268996
    Reviewed by Mike Wyrzykowski and Charlie Wolfe.
    
    Mechanical migration from Ref { expr } to protect(expr) in WebKit's shared
    WebGPU serialization layer, aligning with the codebase-wide transition away
    from brace-initialized smart pointer temporaries.
    
    No new tests needed (no behavioral change, style-only refactor).
    
    * Source/WebKit/Shared/WebGPU/WebGPUBindGroupEntry.cpp:
    (WebKit::WebGPU::ConvertToBackingContext::convertToBacking):
    
    Canonical link: https://commits.webkit.org/310794@main

--- Source/WebKit/Shared/WebGPU/WebGPUBindGroupEntry.cpp
+++ Source/WebKit/Shared/WebGPU/WebGPUBindGroupEntry.cpp
@@ -40,15 +40,15 @@ namespace WebKit::WebGPU {
 std::optional<BindGroupEntry> ConvertToBackingContext::convertToBacking(const WebCore::WebGPU::BindGroupEntry& bindGroupEntry)
 {
     return WTF::switchOn(bindGroupEntry.resource, [&] (std::reference_wrapper<WebCore::WebGPU::Sampler> sampler) -> std::optional<BindGroupEntry> {
-        auto identifier = convertToBacking(Ref { sampler.get() }.get());
+        auto identifier = convertToBacking(protect(sampler.get()).get());
 
         return { { bindGroupEntry.binding, { identifier }, identifier, BindingResourceType::Sampler } };
     }, [&] (std::reference_wrapper<WebCore::WebGPU::Texture> texture) -> std::optional<BindGroupEntry> {
-        auto identifier = convertToBacking(Ref { texture.get() }.get());
+        auto identifier = convertToBacking(protect(texture.get()).get());
 
         return { { bindGroupEntry.binding, { identifier }, identifier, BindingResourceType::Texture } };
     }, [&] (std::reference_wrapper<WebCore::WebGPU::TextureView> textureView) -> std::optional<BindGroupEntry> {
-        auto identifier = convertToBacking(Ref { textureView.get() }.get());
+        auto identifier = convertToBacking(protect(textureView.get()).get());
 
         return { { bindGroupEntry.binding, { identifier }, identifier, BindingResourceType::TextureView } };
     }, [&] (const auto& bufferBinding) -> std::optional<BindGroupEntry> {
@@ -58,7 +58,7 @@ std::optional<BindGroupEntry> ConvertToBackingContext::convertToBacking(const We
 
         return { { bindGroupEntry.binding, WTF::move(*convertedBufferBinding), convertedBufferBinding->buffer, BindingResourceType::BufferBinding } };
     }, [&] (std::reference_wrapper<WebCore::WebGPU::ExternalTexture> externalTexture) -> std::optional<BindGroupEntry> {
-        auto identifier = convertToBacking(Ref { externalTexture.get() }.get());
+        auto identifier = convertToBacking(protect(externalTexture.get()).get());
 
         return { { bindGroupEntry.binding, { identifier }, identifier, BindingResourceType::ExternalTexture } };
     });
