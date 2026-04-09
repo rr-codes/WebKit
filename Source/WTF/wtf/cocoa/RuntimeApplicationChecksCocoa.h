595877325542edd0b428c38b1aa03fa2752d3977
Ben Nham <nham@apple.com>
2026-04-09 10:11:09 -0700

    Add LOOA check for user script and user style interning https://bugs.webkit.org/show_bug.cgi?id=311658 rdar://174250486
    Reviewed by Per Arne Vollan.
    
    Some apps create WKUserScript and WKUserStyleSheet objects off the main thread. This was never safe,
    but clients could sometimes get away with this thread safety issue in the past. But apps are much
    less likely to get away with this issue after 310050@main, since that patch interns source strings
    in to a non-thread-safe HashCountedSet.
    
    To give devs some time to get used to this behavior, put the interning behind a LOOA check.
    
    * Source/WTF/wtf/cocoa/RuntimeApplicationChecksCocoa.h:
    * Source/WebCore/page/UserScript.cpp:
    (WebCore::crashDueToApplicationCreatingUserScriptFromBackgroundThread):
    (WebCore::internedSourceString):
    (WebCore::removeSourceString):
    (WebCore::UserScript::~UserScript):
    * Source/WebCore/page/UserStyleSheet.cpp:
    (WebCore::crashDueToApplicationCreatingUserStyleSheetFromBackgroundThread):
    (WebCore::internedStyleString):
    (WebCore::removeStyleString):
    (WebCore::UserStyleSheet::~UserStyleSheet):
    * Source/WebKit/UIProcess/UserContent/WebUserContentControllerProxy.cpp:
    (WebKit::WebUserContentControllerProxy::cachedTransferString const):
    
    Canonical link: https://commits.webkit.org/310850@main

--- Source/WTF/wtf/cocoa/RuntimeApplicationChecksCocoa.h
+++ Source/WTF/wtf/cocoa/RuntimeApplicationChecksCocoa.h
@@ -139,6 +139,7 @@ enum class SDKAlignedBehavior {
     SuppressKeypressForModifierShortcuts,
     ScrollColorExtensionGrowsDuringRubberBanding,
     ManagedRefreshControlAppearance,
+    EnableUserScriptAndUserStyleInterning,
 
     NumberOfBehaviors
 };
