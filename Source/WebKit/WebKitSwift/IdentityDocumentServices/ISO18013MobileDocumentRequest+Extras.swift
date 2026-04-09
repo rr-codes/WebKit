rr-codes/WebKit

commit 5c47ee43ba73f3b00046dcc6bdd6212ad06e6296

Replace HAVE_DC_ISSUER_IDENTIFIER_SUPPORT with relevant canImport condition instead https://bugs.webkit.org/show_bug.cgi?id=310716 rdar://173331794
Reviewed by Abrar Rahman Protyasha.

Rather than having a new compile time flag introduced, we should just leverage canImport with a version instead.

* Source/WebKit/WebKitSwift/IdentityDocumentServices/ISO18013MobileDocumentRequest+Extras.swift:
* Source/WebKit/WebKitSwift/IdentityDocumentServices/WKIdentityDocumentPresentmentMobileDocumentRequest+Extras.swift:

Canonical link: https://commits.webkit.org/310264@main

Source/WebKit/WebKitSwift/IdentityDocumentServices/ISO18013MobileDocumentRequest+Extras.swift
@@ -34,7 +34,7 @@ extension ISO18013MobileDocumentRequest.ElementInfo {
 
 extension ISO18013MobileDocumentRequest.DocumentRequest {
     init(_ source: WKIdentityDocumentPresentmentMobileDocumentIndividualDocumentRequest) {
-        #if HAVE_DC_ISSUER_IDENTIFIER_SUPPORT
+        #if canImport(IdentityDocumentServices, _version: 9.22.2)
         self = .init(
             documentType: source.documentType,
             namespaces: source.namespaces.mapValues {

Source/WebKit/WebKitSwift/IdentityDocumentServices/WKIdentityDocumentPresentmentMobileDocumentRequest+Extras.swift
@@ -34,7 +34,7 @@ extension WKIdentityDocumentPresentmentMobileDocumentElementInfo {
 
 extension WKIdentityDocumentPresentmentMobileDocumentIndividualDocumentRequest {
     convenience init(_ source: ISO18013MobileDocumentRequest.DocumentRequest) {
-        #if HAVE_DC_ISSUER_IDENTIFIER_SUPPORT
+        #if canImport(IdentityDocumentServices, _version: 9.22.2)
         self.init(
             documentType: source.documentType,
             namespaces: source.namespaces.mapValues {

commit 8423da059d4d64690a75c8c99e29e62ba44c8a37

Some internal builds still broken after 310264@main (missing DC issuer identifier support) https://bugs.webkit.org/show_bug.cgi?id=311867 rdar://174439043
Unreviewed build fix.

It is possible to build in certain configurations where the SDK has
IdentityDocumentServices-9.22.2, but constituent API may still not be
available for the deployment target.

To work around this, we simply compile out usage of such API when
necessary.

* Source/WebKit/WebKitSwift/IdentityDocumentServices/ISO18013MobileDocumentRequest+Extras.swift:
* Source/WebKit/WebKitSwift/IdentityDocumentServices/WKIdentityDocumentPresentmentMobileDocumentRequest+Extras.swift:

Canonical link: https://commits.webkit.org/310869@main

Source/WebKit/WebKitSwift/IdentityDocumentServices/ISO18013MobileDocumentRequest+Extras.swift
@@ -34,7 +34,7 @@ extension ISO18013MobileDocumentRequest.ElementInfo {
 
 extension ISO18013MobileDocumentRequest.DocumentRequest {
     init(_ source: WKIdentityDocumentPresentmentMobileDocumentIndividualDocumentRequest) {
-        #if canImport(IdentityDocumentServices, _version: 9.22.2)
+        #if HAVE_DC_ISSUER_IDENTIFIER_SUPPORT && canImport(IdentityDocumentServices, _version: 9.22.2)
         self = .init(
             documentType: source.documentType,
             namespaces: source.namespaces.mapValues {

Source/WebKit/WebKitSwift/IdentityDocumentServices/WKIdentityDocumentPresentmentMobileDocumentRequest+Extras.swift
@@ -34,7 +34,7 @@ extension WKIdentityDocumentPresentmentMobileDocumentElementInfo {
 
 extension WKIdentityDocumentPresentmentMobileDocumentIndividualDocumentRequest {
     convenience init(_ source: ISO18013MobileDocumentRequest.DocumentRequest) {
-        #if canImport(IdentityDocumentServices, _version: 9.22.2)
+        #if HAVE_DC_ISSUER_IDENTIFIER_SUPPORT && canImport(IdentityDocumentServices, _version: 9.22.2)
         self.init(
             documentType: source.documentType,
             namespaces: source.namespaces.mapValues {