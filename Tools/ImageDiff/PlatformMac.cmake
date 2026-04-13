6f2eaeeeae5d9f7779899fa07cbaaf5a05c75bc8
Pascoe <pascoe@apple.com>
2026-04-13 12:21:20 -0700

    [CMake] Fix Mac CMake build for Tools https://bugs.webkit.org/show_bug.cgi?id=312027 rdar://problem/174581635
    Reviewed by Brandon Stewart.
    
    Add Mac-specific CMake configuration for ImageDiff, MiniBrowser,
    WebKitTestRunner, and TestWebKitAPI including framework linking,
    InjectedBundle setup, and test infrastructure.
    
    Enable ENABLE_API_TESTS to build TestWebKitAPI. Add PlatformMac.cmake
    with generic main.cpp entry point for all five test targets, Cocoa
    framework linking, framework header include paths, and .bundle
    configuration for the InjectedBundle (MODULE library type on APPLE).
    
    * Source/cmake/OptionsMac.cmake: Enable ENABLE_API_TESTS.
    * Tools/ImageDiff/PlatformMac.cmake:
    * Tools/MiniBrowser/mac/CMakeLists.txt:
    * Tools/PlatformMac.cmake:
    * Tools/TestWebKitAPI/CMakeLists.txt: Use MODULE library type for
    InjectedBundle on APPLE so PlatformMac.cmake can configure it as a
    macOS .bundle.
    * Tools/TestWebKitAPI/PlatformMac.cmake: Add generic/main.cpp entry
    point, Cocoa framework linking, framework header include directories,
    .bundle InjectedBundle with WTF/WebCoreTestSupport linking.
    * Tools/WebKitTestRunner/CMakeLists.txt:
    * Tools/WebKitTestRunner/PlatformMac.cmake:
    
    Canonical link: https://commits.webkit.org/311132@main

--- Tools/ImageDiff/PlatformMac.cmake
+++ Tools/ImageDiff/PlatformMac.cmake
@@ -1,1 +1,16 @@
-include(CoreGraphics.cmake)
+list(APPEND ImageDiff_SOURCES
+    cg/PlatformImageCG.cpp
+)
+
+# FIXME: CoreGraphics.cmake's FindApple.cmake targets don't resolve native macOS framework
+# headers -- use -framework directly. https://bugs.webkit.org/show_bug.cgi?id=312069
+list(APPEND ImageDiff_LIBRARIES
+    "-framework CoreFoundation"
+    "-framework CoreGraphics"
+    "-framework CoreServices"  # kUTTypePNG
+    "-framework CoreText"
+    "-framework ImageIO"
+)
+
+# kUTTypePNG deprecated in 12.0; our deployment target is 26.2.
+list(APPEND ImageDiff_COMPILE_OPTIONS -Wno-deprecated-declarations)
