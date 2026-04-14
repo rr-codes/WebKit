b4a487e92d0a24f749d597faf11a53655e4abeb6
Marta Darbinyan <darbinyan@apple.com>
2026-04-14 12:34:00 -0700

    [Gardening]: [macOS Debug] TestWebKitAPI.SOAuthorizationPopUp.SOAuthorizationLoadPolicyAllowAsync is a flaky failure https://bugs.webkit.org/show_bug.cgi?id=312290 rdar://174759296
    Unreviewed test Gardening
    
    Skipping the flaky test in macOS Debug.
    
    * Tools/TestWebKitAPI/Tests/WebKit/WKWebView/SOAuthorizationTests.mm:
    (TestWebKitAPI::TEST(SOAuthorizationRedirect, SOAuthorizationLoadPolicyAllowAsync)):
    
    Canonical link: https://commits.webkit.org/311218@main

--- Tools/TestWebKitAPI/Tests/WebKit/WKWebView/SOAuthorizationTests.mm
+++ Tools/TestWebKitAPI/Tests/WebKit/WKWebView/SOAuthorizationTests.mm
@@ -1633,7 +1633,12 @@ TEST(SOAuthorizationRedirect, SOAuthorizationLoadPolicyIgnore)
     EXPECT_WK_STREQ(testURL.get().absoluteString, finalURL);
 }
 
+// FIXME when webkit.org/b/312290 is resolved.
+#if PLATFORM(MAC) && !defined(NDEBUG)
+TEST(SOAuthorizationRedirect, DISABLED_SOAuthorizationLoadPolicyAllowAsync)
+#else
 TEST(SOAuthorizationRedirect, SOAuthorizationLoadPolicyAllowAsync)
+#endif
 {
     resetState();
     SWIZZLE_SOAUTH(PAL::getSOAuthorizationClassSingleton());
