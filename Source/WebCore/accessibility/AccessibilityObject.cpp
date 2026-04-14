b6c85acbb9b05bac00f9eace59c02ec375146005
Tyler Wilcock <tyler_w@apple.com>
2026-04-13 18:51:58 -0700

    AX: AccessibilityObject::lineRangeForPosition can loop infinitely with display:table in flex container https://bugs.webkit.org/show_bug.cgi?id=312173 rdar://174675164
    Reviewed by Joshua Hoffman.
    
    The while loop in lineRangeForPosition advances by calling VisiblePosition::next(),
    but if next() returns the same position (e.g., at a boundary in certain DOM structures
    like a display:table span inside a flex container), the loop never terminates. Add a
    check to break when no forward progress is made.
    
    * LayoutTests/accessibility/line-range-display-table-in-flex-expected.txt: Added.
    * LayoutTests/accessibility/line-range-display-table-in-flex.html: Added.
    * LayoutTests/platform/ios/TestExpectations:
    * Source/WebCore/accessibility/AccessibilityObject.cpp:
    (WebCore::AccessibilityObject::lineRangeForPosition const):
    
    Canonical link: https://commits.webkit.org/311153@main

--- Source/WebCore/accessibility/AccessibilityObject.cpp
+++ Source/WebCore/accessibility/AccessibilityObject.cpp
@@ -1902,6 +1902,11 @@ VisiblePositionRange AccessibilityObject::lineRangeForPosition(const VisiblePosi
     auto end = visiblePosition;
     while (end.isNotNull() && inSameLine(end, visiblePosition)) {
         auto next = end.next();
+        if (next == end) {
+            // Without this break, we would loop infinitely.
+            break;
+        }
+
         if (stringForVisiblePositionRange({ end, next }).contains("\n"_s)) {
             // Return the range including the line break.
             return { start, next };
@@ -2089,7 +2094,12 @@ VisiblePosition AccessibilityObject::nextLineEndPosition(const VisiblePosition& 
     // we may end up back at the same position we started at. This is never valid, so keep moving forward
     // trying to find the next line end.
     while ((lineEndPosition.isNull() || lineEndPosition == startPosition) && nextPosition.isNotNull()) {
+        auto previousPosition = nextPosition;
         nextPosition = nextPosition.next();
+        if (nextPosition == previousPosition) {
+            // Without this break, we would loop infinitely.
+            break;
+        }
         lineEndPosition = endOfLine(nextPosition);
     }
     return lineEndPosition;
@@ -2110,7 +2120,12 @@ std::optional<VisiblePosition> AccessibilityObject::previousLineStartPositionInt
     // This avoids returning a null position when we shouldn't, like when a position is next to a floating object.
     if (startPosition.isNull()) {
         while (startPosition.isNull() && previousVisiblePosition.isNotNull()) {
+            auto previousPosition = previousVisiblePosition;
             previousVisiblePosition = previousVisiblePosition.previous();
+            if (previousVisiblePosition == previousPosition) {
+                // Without this break, we would loop infinitely.
+                break;
+            }
             startPosition = startOfLine(previousVisiblePosition);
         }
     } else
