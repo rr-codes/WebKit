6ada1137f92bbf920f4c657cdb44251f3ea5aa41
Cole Carley <ccarley2@apple.com>
2026-04-14 12:06:53 -0700

    outlook.live.com: Unable to change selected text rdar://151851274 https://bugs.webkit.org/show_bug.cgi?id=311961
    Reviewed by Wenson Hsieh.
    
    When on outlook.live.com looking at a received or sent email on iOS,
    you can create a selection, but it is tedious to change the selected text.
    This is because the outlook JS has an event listener for touch events
    that are used to transform: translate3d the page. In that listener callback
    they call preventDefault, which blocks WebKit from using the touch
    event to move the selection.
    
    To solve this issue, I added a check to touchstart to see if it is near
    a selection handle. If it is, then we set some state on the view, and
    ensure that the touch start cannot prevent native gestures. Then, all
    subsequent touchmove events will not be sent to JS, so they can be used
    to adjust the text selection.
    
    After all fingers are lifted, the state is reset, and touch events will
    go back to normal until another touch start is near a selection handle.
    
    Test: editing/selection/ios/touchmove-to-change-selection-with-prevent-default.html
    
    * LayoutTests/editing/selection/ios/touchmove-to-change-selection-with-prevent-default-expected.txt: Added.
    * LayoutTests/editing/selection/ios/touchmove-to-change-selection-with-prevent-default.html: Added.
    * Source/WebKit/UIProcess/ios/WKContentViewInteraction.h:
    * Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
    (-[WKContentView _touchEventsRecognized]):
    (-[WKContentView _doneDeferringTouchStart:]):
    (-[WKContentView _doneDeferringTouchMove:]):
    (-[WKContentView _isTouchNearSelectionHandle:]):
    
    Canonical link: https://commits.webkit.org/311216@main

--- Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
+++ Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -2229,6 +2229,9 @@ typedef NS_ENUM(NSInteger, EndEditingReason) {
         [self doAfterPositionInformationUpdate:[assistant = WeakObjCPtr<WKActionSheetAssistant>(_actionSheetAssistant.get())] (WebKit::InteractionInformationAtPosition information) {
             [assistant.get() interactionDidStartWithPositionInformation:information];
         } forRequest:positionInformationRequest];
+
+        if (_touchEventsCanPreventNativeGestures && [self _isTouchNearSelectionHandle:lastTouchEvent.locationInRootViewCoordinates])
+            _touchStartedNearSelectionHandle = YES;
     }
 
 #if ENABLE(TOUCH_EVENTS)
@@ -2237,13 +2240,17 @@ typedef NS_ENUM(NSInteger, EndEditingReason) {
 
     [self _handleTouchActionsForTouchEvent:nativeWebTouchEvent];
 
-    if (_touchEventsCanPreventNativeGestures)
+    if (_touchStartedNearSelectionHandle && lastTouchEvent.type != WebKit::WKTouchEventType::Begin) {
+        if (lastTouchEvent.type == WebKit::WKTouchEventType::Change || lastTouchEvent.type == WebKit::WKTouchEventType::End)
+            [self _doneDeferringTouchMove:NO];
+    } else if (_touchEventsCanPreventNativeGestures)
         _page->handlePreventableTouchEvent(nativeWebTouchEvent);
     else
         _page->handleUnpreventableTouchEvent(nativeWebTouchEvent);
 
     if (nativeWebTouchEvent.allTouchPointsAreReleased()) {
         _touchEventsCanPreventNativeGestures = YES;
+        _touchStartedNearSelectionHandle = NO;
 
         if (!_page->isScrollingOrZooming())
             [self _resetPanningPreventionFlags];
@@ -3385,6 +3392,38 @@ ALLOW_DEPRECATED_DECLARATIONS_END
     return _page->dataDetectionResults();
 }
 #endif
+
+- (BOOL)_isTouchNearSelectionHandle:(WebCore::FloatPoint)touchPoint
+{
+    if (_lastSelectionDrawingInfo.type != WebCore::SelectionType::Range)
+        return NO;
+
+    if (_suppressSelectionAssistantReasons)
+        return NO;
+
+    if (![_textInteractionWrapper areSelectionHandlesVisible])
+        return NO;
+
+    RefPtr page = _page;
+    if (!page)
+        return NO;
+
+    auto& editorState = page->editorState();
+    if (!editorState.visualData)
+        return NO;
+
+    static constexpr float handleHitTestPadding = 44;
+    auto inflatedContainsPoint = [&](WebCore::IntRect caretRect) -> bool {
+        if (caretRect.isEmpty())
+            return false;
+        WebCore::FloatRect hitArea(caretRect);
+        hitArea.inflate(handleHitTestPadding);
+        return hitArea.contains(touchPoint);
+    };
+
+    return inflatedContainsPoint(editorState.visualData->caretRectAtStart)
+        || inflatedContainsPoint(editorState.visualData->caretRectAtEnd);
+}
 
 - (BOOL)_pointIsInsideSelectionRect:(CGPoint)point outBoundingRect:(WebCore::FloatRect *)outBoundingRect
 {
@@ -10562,6 +10601,9 @@ static WebCore::DataOwnerType coreDataOwnerType(_UIDataOwner platformType)
         return NO;
 
     if (gestureRecognizer == _touchEventGestureRecognizer)
+        return NO;
+
+    if (_touchStartedNearSelectionHandle)
         return NO;
 
 #if HAVE(UIKIT_WITH_MOUSE_SUPPORT)
