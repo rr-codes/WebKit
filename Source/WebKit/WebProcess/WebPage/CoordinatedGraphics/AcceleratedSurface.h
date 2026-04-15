rr-codes/WebKit

commit 4ca156ae3080e424f8c1c7237b43d39235dbfdf5

[GTK][WPE] Consolidate dma-buf / EGL image creation code into DMABufBuffer methods https://bugs.webkit.org/show_bug.cgi?id=311251
Reviewed by Adrian Perez de Castro.

Eliminate duplicated ADD_PLANE_ATTRIBUTES macros across many call sites by
introducing shared helpers: DMABufBufferAttributes::constructFromGBMBufferObject
for GBM buffer attribute extraction, and DMABufBuffer::createEGLImage overloads
for both GLDisplay and raw EGLDisplay (ANGLE) contexts.

Extract DMABufBufferAttributes into its own header without USE(GBM) guard
so non-GBM builds can use the struct as well.

Also modernize WPEBufferDMABuf.cpp to use the same std::array + loop
pattern. As a WPEPlatform class it cannot use the WebCore functionality
but it should still use the same approach as implemented in
DMABufBufferAttributes.

Covered by existing tests.

* Source/WTF/wtf/unix/UnixFileDescriptor.h:
(WTF::UnixFileDescriptor::UnixFileDescriptor):
(WTF::UnixFileDescriptor::~UnixFileDescriptor):
* Source/WebCore/platform/TextureMapper.cmake:
* Source/WebCore/platform/graphics/gbm/DMABufBuffer.cpp:
(WebCore::DMABufBuffer::DMABufBuffer):
(WebCore::DMABufBufferAttributes::fromGBMBufferObject):
(WebCore::DMABufBuffer::createEGLImage const):
(WebCore::buildEGLAttributesForDMABuf):
(WebCore::DMABufBuffer::createEGLImage):
* Source/WebCore/platform/graphics/gbm/DMABufBuffer.h:
* Source/WebCore/platform/graphics/gbm/DMABufBufferAttributes.h: Copied from Source/WebCore/platform/graphics/gbm/DMABufBuffer.cpp.
* Source/WebCore/platform/graphics/gbm/GraphicsContextGLTextureMapperGBM.cpp:
(WebCore::GraphicsContextGLTextureMapperGBM::createDrawingBuffer const):
(WebCore::GraphicsContextGLTextureMapperGBM::createExternalImage):
* Source/WebCore/platform/graphics/gbm/MemoryMappedGPUBuffer.cpp:
(WebCore::MemoryMappedGPUBuffer::createDMABufFromGBMBufferObject):
(WebCore::MemoryMappedGPUBuffer::createEGLImageFromDMABuf):
(WebCore::MemoryMappedGPUBuffer::exportGBMBufferObjectAsDMABuf): Deleted.

Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AcceleratedSurface.h
@@ -49,6 +49,7 @@ WTF_IGNORE_WARNINGS_IN_THIRD_PARTY_CODE_END
 #endif
 
 #if USE(GBM)
+#include <WebCore/DMABufBuffer.h>
 #include <WebCore/DRMDevice.h>
 #include <WebCore/GBMDevice.h>
 struct gbm_bo;
@@ -267,7 +268,7 @@ private:
     class RenderTargetEGLImage final : public RenderTargetShareableBuffer {
     public:
         static std::unique_ptr<RenderTarget> create(AcceleratedSurface&, const WebCore::IntSize&, const BufferFormat&);
-        RenderTargetEGLImage(AcceleratedSurface&, const WebCore::IntSize&, EGLImage, uint32_t format, Vector<WTF::UnixFileDescriptor>&&, Vector<uint32_t>&& offsets, Vector<uint32_t>&& strides, uint64_t modifier, RendererBufferFormat::Usage);
+        RenderTargetEGLImage(AcceleratedSurface&, const WebCore::IntSize&, EGLImage, WebCore::DMABufBufferAttributes&&, RendererBufferFormat::Usage);
 #if OS(ANDROID)
         RenderTargetEGLImage(AcceleratedSurface&, const WebCore::IntSize&, EGLImage, RefPtr<AHardwareBuffer>&&);
 #endif

Source/WTF/wtf/unix/UnixFileDescriptor.h
@@ -46,6 +46,12 @@ public:
         : m_value(fd)
     { }
 
+    enum BorrowTag { Borrow };
+    UnixFileDescriptor(int fd, BorrowTag)
+        : m_value(fd)
+        , m_shouldClose(false)
+    { }
+
     enum DuplicationTag { Duplicate };
     UnixFileDescriptor(int fd, DuplicationTag)
     {
@@ -70,7 +76,7 @@ public:
 
     ~UnixFileDescriptor()
     {
-        if (m_value >= 0)
+        if (m_value >= 0 && m_shouldClose)
             closeWithRetry(std::exchange(m_value, -1));
     }
 
@@ -87,6 +93,7 @@ public:
 
 private:
     int m_value { -1 };
+    bool m_shouldClose : 1 { true };
 };
 
 } // namespace WTF

Source/WebCore/platform/TextureMapper.cmake
@@ -88,6 +88,7 @@ if (USE_COORDINATED_GRAPHICS)
     list(APPEND WebCore_PRIVATE_FRAMEWORK_HEADERS
         page/scrolling/coordinated/ScrollingTreeCoordinated.h
 
+        platform/graphics/gbm/DMABufBufferAttributes.h
         platform/graphics/texmap/coordinated/CoordinatedAnimatedBackingStoreClient.h
         platform/graphics/texmap/coordinated/CoordinatedBackingStore.h
         platform/graphics/texmap/coordinated/CoordinatedBackingStoreProxy.h

Source/WebCore/platform/graphics/gbm/DMABufBuffer.cpp
@@ -28,7 +28,17 @@
 
 #if USE(GBM)
 #include "CoordinatedPlatformLayerBuffer.h"
+#include "GBMVersioning.h"
+#include "GLDisplay.h"
 #include <atomic>
+#include <drm_fourcc.h>
+
+#if USE(LIBEPOXY)
+#include <epoxy/egl.h>
+#else
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#endif
 
 namespace WebCore {
 
@@ -38,9 +48,9 @@ static uint64_t generateID()
     return ++id;
 }
 
-DMABufBuffer::DMABufBuffer(const IntSize& size, uint32_t fourcc, Vector<WTF::UnixFileDescriptor>&& fds, Vector<uint32_t>&& offsets, Vector<uint32_t>&& strides, uint64_t modifier)
+DMABufBuffer::DMABufBuffer(Attributes&& attributes)
     : m_id(generateID())
-    , m_attributes({ size, fourcc, WTF::move(fds), WTF::move(offsets), WTF::move(strides), modifier })
+    , m_attributes(WTF::move(attributes))
 {
 }
 
@@ -63,6 +73,119 @@ std::optional<DMABufBuffer::Attributes> DMABufBuffer::takeAttributes()
         return std::nullopt;
 
     return DMABufBuffer::Attributes { WTF::move(m_attributes.size), std::exchange(m_attributes.fourcc, 0), WTF::move(m_attributes.fds), WTF::move(m_attributes.offsets), WTF::move(m_attributes.strides), std::exchange(m_attributes.modifier, 0) };
+}
+
+std::optional<DMABufBufferAttributes> DMABufBufferAttributes::fromGBMBufferObject(struct gbm_bo* bo, EnableModifiers enableModifiers)
+{
+    if (!bo)
+        return std::nullopt;
+
+    int planeCount = gbm_bo_get_plane_count(bo);
+    if (planeCount <= 0)
+        return std::nullopt;
+
+    DMABufBufferAttributes attributes;
+    attributes.size = { static_cast<int>(gbm_bo_get_width(bo)), static_cast<int>(gbm_bo_get_height(bo)) };
+    attributes.fourcc = gbm_bo_get_format(bo);
+    attributes.modifier = enableModifiers == EnableModifiers::Yes ? gbm_bo_get_modifier(bo) : DRM_FORMAT_MOD_INVALID;
+
+    for (int i = 0; i < planeCount; ++i) {
+        int fd = gbm_bo_get_fd_for_plane(bo, i);
+        if (fd < 0) {
+            LOG_ERROR("DMABufBufferAttributes::fromGBMBufferObject(), failed to export dma-buf for plane %d", i);
+            return std::nullopt;
+        }
+        attributes.fds.append(UnixFileDescriptor { fd, UnixFileDescriptor::Adopt });
+        attributes.offsets.append(gbm_bo_get_offset(bo, i));
+        attributes.strides.append(gbm_bo_get_stride_for_plane(bo, i));
+    }
+
+    return attributes;
+}
+
+EGLImage DMABufBuffer::createEGLImage(GLDisplay& display) const
+{
+    return createEGLImage(display, m_attributes);
+}
+
+static std::optional<Vector<EGLAttrib>> buildEGLAttributesForDMABuf(const DMABufBuffer::Attributes& dmaBufAttributes, DMABufBuffer::Attributes::EnableModifiers enableModifiers)
+{
+    auto planeCount = dmaBufAttributes.fds.size();
+    if (!planeCount || planeCount > DMABufBuffer::Attributes::maxPlaneCountForEGLImage)
+        return std::nullopt;
+
+    bool hasModifiers = dmaBufAttributes.modifier != DRM_FORMAT_MOD_INVALID && enableModifiers == DMABufBuffer::Attributes::EnableModifiers::Yes;
+
+    // 6 base attributes + per-plane (6 + optional 4 modifier) + EGL_NONE terminator.
+    static constexpr unsigned baseAttributeCount = 6;
+    static constexpr unsigned planeAttributeCount = 6;
+    static constexpr unsigned modifierAttributeCount = 4;
+
+    Vector<EGLAttrib> eglAttributes;
+    eglAttributes.reserveInitialCapacity(baseAttributeCount + planeCount * (planeAttributeCount + (hasModifiers ? modifierAttributeCount : 0)) + 1);
+
+    eglAttributes.appendList<EGLAttrib>({
+        EGL_WIDTH, static_cast<EGLAttrib>(dmaBufAttributes.size.width()),
+        EGL_HEIGHT, static_cast<EGLAttrib>(dmaBufAttributes.size.height()),
+        EGL_LINUX_DRM_FOURCC_EXT, static_cast<EGLAttrib>(dmaBufAttributes.fourcc.value)
+    });
+
+    static constexpr std::array planeAttributeNames = {
+        std::array { EGL_DMA_BUF_PLANE0_FD_EXT, EGL_DMA_BUF_PLANE0_OFFSET_EXT, EGL_DMA_BUF_PLANE0_PITCH_EXT, EGL_DMA_BUF_PLANE0_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE0_MODIFIER_LO_EXT },
+        std::array { EGL_DMA_BUF_PLANE1_FD_EXT, EGL_DMA_BUF_PLANE1_OFFSET_EXT, EGL_DMA_BUF_PLANE1_PITCH_EXT, EGL_DMA_BUF_PLANE1_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE1_MODIFIER_LO_EXT },
+        std::array { EGL_DMA_BUF_PLANE2_FD_EXT, EGL_DMA_BUF_PLANE2_OFFSET_EXT, EGL_DMA_BUF_PLANE2_PITCH_EXT, EGL_DMA_BUF_PLANE2_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE2_MODIFIER_LO_EXT },
+        std::array { EGL_DMA_BUF_PLANE3_FD_EXT, EGL_DMA_BUF_PLANE3_OFFSET_EXT, EGL_DMA_BUF_PLANE3_PITCH_EXT, EGL_DMA_BUF_PLANE3_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE3_MODIFIER_LO_EXT },
+    };
+
+    for (size_t i = 0; i < planeCount; ++i) {
+        const auto& names = planeAttributeNames[i];
+        eglAttributes.appendList<EGLAttrib>({
+            names[0], static_cast<EGLAttrib>(dmaBufAttributes.fds[i].value()),
+            names[1], static_cast<EGLAttrib>(dmaBufAttributes.offsets[i]),
+            names[2], static_cast<EGLAttrib>(dmaBufAttributes.strides[i])
+        });
+
+        if (hasModifiers) {
+            eglAttributes.appendList<EGLAttrib>({
+                names[3], static_cast<EGLAttrib>(dmaBufAttributes.modifier >> 32),
+                names[4], static_cast<EGLAttrib>(dmaBufAttributes.modifier & 0xffffffff)
+            });
+        }
+    }
+
+    eglAttributes.append(EGL_NONE);
+    return eglAttributes;
+}
+
+EGLImage DMABufBuffer::createEGLImage(EGLDisplay eglDisplay) const
+{
+    return createEGLImage(eglDisplay, m_attributes);
+}
+
+EGLImage DMABufBuffer::createEGLImage(GLDisplay& display, const Attributes& dmaBufAttributes)
+{
+    auto enableModifiers = display.extensions().EXT_image_dma_buf_import_modifiers
+        ? DMABufBuffer::Attributes::EnableModifiers::Yes : DMABufBuffer::Attributes::EnableModifiers::No;
+    auto eglAttributes = buildEGLAttributesForDMABuf(dmaBufAttributes, enableModifiers);
+    if (!eglAttributes)
+        return EGL_NO_IMAGE;
+    return display.createImage(EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, nullptr, *eglAttributes);
+}
+
+EGLImage DMABufBuffer::createEGLImage(EGLDisplay eglDisplay, const Attributes& dmaBufAttributes)
+{
+    auto eglAttributes = buildEGLAttributesForDMABuf(dmaBufAttributes, DMABufBuffer::Attributes::EnableModifiers::Yes);
+    if (!eglAttributes || eglDisplay == EGL_NO_DISPLAY)
+        return EGL_NO_IMAGE;
+
+    static PFNEGLCREATEIMAGEKHRPROC s_eglCreateImageKHR = reinterpret_cast<PFNEGLCREATEIMAGEKHRPROC>(eglGetProcAddress("eglCreateImageKHR"));
+    if (!s_eglCreateImageKHR)
+        return EGL_NO_IMAGE;
+
+    auto intAttributes = eglAttributes->map<Vector<EGLint>>([](EGLAttrib value) {
+        return static_cast<EGLint>(value);
+    });
+    return s_eglCreateImageKHR(eglDisplay, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, nullptr, intAttributes.span().data());
 }
 
 } // namespace WebCore

Source/WebCore/platform/graphics/gbm/DMABufBuffer.h
@@ -26,32 +26,24 @@
 #pragma once
 
 #if USE(COORDINATED_GRAPHICS) && USE(GBM)
-#include "IntSize.h"
-#include <optional>
+#include "DMABufBufferAttributes.h"
 #include <wtf/ThreadSafeRefCounted.h>
-#include <wtf/Vector.h>
-#include <wtf/unix/UnixFileDescriptor.h>
+
+typedef void* EGLDisplay;
+typedef void* EGLImage;
 
 namespace WebCore {
 
 class CoordinatedPlatformLayerBuffer;
-
-struct DMABufBufferAttributes {
-    IntSize size;
-    uint32_t fourcc { 0 };
-    Vector<WTF::UnixFileDescriptor> fds;
-    Vector<uint32_t> offsets;
-    Vector<uint32_t> strides;
-    uint64_t modifier { 0 };
-};
+class GLDisplay;
 
 class DMABufBuffer final : public ThreadSafeRefCounted<DMABufBuffer> {
 public:
     using Attributes = DMABufBufferAttributes;
 
-    static Ref<DMABufBuffer> create(const IntSize& size, uint32_t fourcc, Vector<WTF::UnixFileDescriptor>&& fds, Vector<uint32_t>&& offsets, Vector<uint32_t>&& strides, uint64_t modifier)
+    static Ref<DMABufBuffer> create(Attributes&& attributes)
     {
-        return adoptRef(*new DMABufBuffer(size, fourcc, WTF::move(fds), WTF::move(offsets), WTF::move(strides), modifier));
+        return adoptRef(*new DMABufBuffer(WTF::move(attributes)));
     }
     static Ref<DMABufBuffer> create(uint64_t id, Attributes&& attributes)
     {
@@ -71,11 +63,16 @@ public:
     std::optional<TransferFunction> transferFunction() const { return m_transferFunction; }
     void setTransferFunction(TransferFunction transferFunction) { m_transferFunction = transferFunction; }
 
+    EGLImage createEGLImage(GLDisplay&) const;
+    EGLImage createEGLImage(EGLDisplay) const;
+    static EGLImage createEGLImage(GLDisplay&, const Attributes&);
+    static EGLImage createEGLImage(EGLDisplay, const Attributes&);
+
     CoordinatedPlatformLayerBuffer* buffer() const LIFETIME_BOUND { return m_buffer.get(); }
     void setBuffer(std::unique_ptr<CoordinatedPlatformLayerBuffer>&&);
 
 private:
-    DMABufBuffer(const IntSize&, uint32_t fourcc, Vector<WTF::UnixFileDescriptor>&&, Vector<uint32_t>&&, Vector<uint32_t>&&, uint64_t modifier);
+    explicit DMABufBuffer(Attributes&&);
     DMABufBuffer(uint64_t id, Attributes&&);
 
     uint64_t m_id { 0 };

Source/WebCore/platform/graphics/gbm/DMABufBufferAttributes.h
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2024, 2026 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#if USE(COORDINATED_GRAPHICS)
+#include "FourCC.h"
+#include "IntSize.h"
+#include <optional>
+#include <wtf/Vector.h>
+#include <wtf/unix/UnixFileDescriptor.h>
+
+#if USE(GBM)
+struct gbm_bo;
+#endif
+
+namespace WebCore {
+
+class DMABufBufferAttributes {
+public:
+    // EGL_EXT_image_dma_buf_import supports up to 4 planes (PLANE0..PLANE3).
+    static constexpr unsigned maxPlaneCountForEGLImage = 4;
+
+    IntSize size;
+    FourCC fourcc;
+    Vector<WTF::UnixFileDescriptor> fds;
+    Vector<uint32_t> offsets;
+    Vector<uint32_t> strides;
+    uint64_t modifier { 0 };
+
+#if USE(GBM)
+    enum class EnableModifiers : bool { No, Yes };
+    static std::optional<DMABufBufferAttributes> fromGBMBufferObject(struct gbm_bo*, EnableModifiers = EnableModifiers::Yes);
+#endif
+};
+
+} // namespace WebCore
+
+#endif // USE(COORDINATED_GRAPHICS)

Source/WebCore/platform/graphics/gbm/GraphicsContextGLTextureMapperGBM.cpp
@@ -124,65 +124,29 @@ GraphicsContextGLTextureMapperGBM::DrawingBuffer GraphicsContextGLTextureMapperG
 
     const auto size = getInternalFramebufferSize();
     struct gbm_bo* bo = nullptr;
-    bool disableModifiers = m_drawingBufferFormat.modifiers.size() == 1 && m_drawingBufferFormat.modifiers[0] == DRM_FORMAT_MOD_INVALID;
-    if (!disableModifiers && !m_drawingBufferFormat.modifiers.isEmpty())
+    auto enableModifiers = m_drawingBufferFormat.modifiers.size() == 1 && m_drawingBufferFormat.modifiers[0] == DRM_FORMAT_MOD_INVALID
+        ? DMABufBufferAttributes::EnableModifiers::No : DMABufBufferAttributes::EnableModifiers::Yes;
+    if (enableModifiers == DMABufBufferAttributes::EnableModifiers::Yes && !m_drawingBufferFormat.modifiers.isEmpty())
         bo = gbm_bo_create_with_modifiers2(gbmDevice->device(), size.width(), size.height(), m_drawingBufferFormat.fourcc, m_drawingBufferFormat.modifiers.span().data(), m_drawingBufferFormat.modifiers.size(), GBM_BO_USE_RENDERING);
     if (!bo)
         bo = gbm_bo_create(gbmDevice->device(), size.width(), size.height(), m_drawingBufferFormat.fourcc, GBM_BO_USE_RENDERING);
     if (!bo)
         return { };
 
-    Vector<UnixFileDescriptor> fds;
-    Vector<uint32_t> offsets;
-    Vector<uint32_t> strides;
-    uint32_t format = gbm_bo_get_format(bo);
-    int planeCount = gbm_bo_get_plane_count(bo);
-    uint64_t modifier = disableModifiers ? DRM_FORMAT_MOD_INVALID : gbm_bo_get_modifier(bo);
-
-    Vector<EGLint> attributes = {
-        EGL_WIDTH, size.width(),
-        EGL_HEIGHT, size.height(),
-        EGL_LINUX_DRM_FOURCC_EXT, static_cast<EGLint>(format),
-    };
-
-#define ADD_PLANE_ATTRIBUTES(planeIndex) { \
-    fds.append(UnixFileDescriptor { gbm_bo_get_fd_for_plane(bo, planeIndex), UnixFileDescriptor::Adopt }); \
-    offsets.append(gbm_bo_get_offset(bo, planeIndex)); \
-    strides.append(gbm_bo_get_stride_for_plane(bo, planeIndex)); \
-    std::array<EGLint, 6> planeAttributes { \
-        EGL_DMA_BUF_PLANE##planeIndex##_FD_EXT, fds.last().value(), \
-        EGL_DMA_BUF_PLANE##planeIndex##_OFFSET_EXT, static_cast<EGLint>(offsets.last()), \
-        EGL_DMA_BUF_PLANE##planeIndex##_PITCH_EXT, static_cast<EGLint>(strides.last()) \
-    }; \
-    attributes.append(std::span<const EGLint> { planeAttributes }); \
-    if (modifier != DRM_FORMAT_MOD_INVALID) { \
-        std::array<EGLint, 4> modifierAttributes { \
-            EGL_DMA_BUF_PLANE##planeIndex##_MODIFIER_HI_EXT, static_cast<EGLint>(modifier >> 32), \
-            EGL_DMA_BUF_PLANE##planeIndex##_MODIFIER_LO_EXT, static_cast<EGLint>(modifier & 0xffffffff) \
-        }; \
-        attributes.append(std::span<const EGLint> { modifierAttributes }); \
-    } \
+    auto dmaBufAttributes = DMABufBufferAttributes::fromGBMBufferObject(bo, enableModifiers);
+    if (!dmaBufAttributes) {
+        gbm_bo_destroy(bo);
+        return { };
     }
 
-    if (planeCount > 0)
-        ADD_PLANE_ATTRIBUTES(0);
-    if (planeCount > 1)
-        ADD_PLANE_ATTRIBUTES(1);
-    if (planeCount > 2)
-        ADD_PLANE_ATTRIBUTES(2);
-    if (planeCount > 3)
-        ADD_PLANE_ATTRIBUTES(3);
-
-#undef ADD_PLANE_ATTRIBUTES
-
-    attributes.append(EGL_NONE);
-
-    auto* image = EGL_CreateImageKHR(m_displayObj, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, nullptr, attributes.span().data());
+    Ref dmaBuf = DMABufBuffer::create(WTF::move(*dmaBufAttributes));
+    auto image = dmaBuf->createEGLImage(m_displayObj);
     gbm_bo_destroy(bo);
+
     if (!image)
         return { };
 
-    return { DMABufBuffer::create(size, format, WTF::move(fds), WTF::move(offsets), WTF::move(strides), modifier), image };
+    return { WTF::move(dmaBuf), image };
 }
 
 void GraphicsContextGLTextureMapperGBM::freeDrawingBuffers()
@@ -288,40 +252,6 @@ void GraphicsContextGLTextureMapperGBM::prepareForDisplayWithFinishedSignal(Func
 GCGLExternalImage GraphicsContextGLTextureMapperGBM::createExternalImage(ExternalImageSource&& source, GCGLenum, GCGLint)
 {
     GraphicsContextGLExternalImageSource imageSource = WTF::move(source);
-    Vector<EGLint> attributes = {
-        EGL_WIDTH, imageSource.size.width(),
-        EGL_HEIGHT, imageSource.size.height(),
-        EGL_LINUX_DRM_FOURCC_EXT, static_cast<EGLint>(imageSource.fourcc)
-    };
-
-#define ADD_PLANE_ATTRIBUTES(planeIndex) { \
-    std::array<EGLAttrib, 6> planeAttributes { \
-        EGL_DMA_BUF_PLANE##planeIndex##_FD_EXT, imageSource.fds[planeIndex].value(), \
-        EGL_DMA_BUF_PLANE##planeIndex##_OFFSET_EXT, static_cast<EGLint>(imageSource.offsets[planeIndex]), \
-        EGL_DMA_BUF_PLANE##planeIndex##_PITCH_EXT, static_cast<EGLint>(imageSource.strides[planeIndex]) \
-    }; \
-    attributes.append(std::span<const EGLAttrib> { planeAttributes }); \
-    if (imageSource.modifier != DRM_FORMAT_MOD_INVALID && PlatformDisplay::sharedDisplay().eglExtensions().EXT_image_dma_buf_import_modifiers) { \
-        std::array<EGLint, 4> modifierAttributes { \
-            EGL_DMA_BUF_PLANE##planeIndex##_MODIFIER_HI_EXT, static_cast<EGLint>(imageSource.modifier >> 32), \
-            EGL_DMA_BUF_PLANE##planeIndex##_MODIFIER_LO_EXT, static_cast<EGLint>(imageSource.modifier & 0xffffffff) \
-        }; \
-        attributes.append(std::span<const EGLint> { modifierAttributes }); \
-    } \
-    };
-    auto planeCount = imageSource.fds.size();
-    if (planeCount > 0)
-        ADD_PLANE_ATTRIBUTES(0);
-    if (planeCount > 1)
-        ADD_PLANE_ATTRIBUTES(1);
-    if (planeCount > 2)
-        ADD_PLANE_ATTRIBUTES(2);
-    if (planeCount > 3)
-        ADD_PLANE_ATTRIBUTES(3);
-
-#undef ADD_PLANE_ATTRIBUTES
-
-    attributes.append(EGL_NONE);
 
     if (m_displayObj == EGL_NO_DISPLAY) {
         addError(GCGLErrorCode::InvalidOperation);
@@ -329,7 +259,8 @@ GCGLExternalImage GraphicsContextGLTextureMapperGBM::createExternalImage(Externa
         return { };
     }
 
-    auto eglImage = EGL_CreateImageKHR(m_displayObj, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, nullptr, attributes.span().data());
+    DMABufBufferAttributes dmaBufAttributes { imageSource.size, imageSource.fourcc, WTF::move(imageSource.fds), WTF::move(imageSource.offsets), WTF::move(imageSource.strides), imageSource.modifier };
+    auto eglImage = DMABufBuffer::createEGLImage(m_displayObj, dmaBufAttributes);
     if (!eglImage) {
         LOG(XR, "invalid operation importing the image %d", EGL_GetError());
         addError(GCGLErrorCode::InvalidOperation);

Source/WebCore/platform/graphics/gbm/MemoryMappedGPUBuffer.cpp
@@ -179,24 +179,14 @@ bool MemoryMappedGPUBuffer::isVivanteSuperTiled() const
 
 bool MemoryMappedGPUBuffer::createDMABufFromGBMBufferObject(struct gbm_bo* bo)
 {
-    Vector<UnixFileDescriptor> fds;
-    Vector<uint32_t> offsets;
-    Vector<uint32_t> strides;
+    auto attributes = DMABufBufferAttributes::fromGBMBufferObject(bo);
+    if (!attributes)
+        return false;
 
-    auto format = gbm_bo_get_format(bo);
-    auto planeCount = gbm_bo_get_plane_count(bo);
-
-    for (int i = 0; i < planeCount; ++i) {
-        if (auto fd = exportGBMBufferObjectAsDMABuf(bo, i))
-            fds.append(WTF::move(fd));
-        else
-            return false;
-        offsets.append(gbm_bo_get_offset(bo, i));
-        strides.append(gbm_bo_get_stride_for_plane(bo, i));
-    }
+    attributes->modifier = m_modifier;
 
     ASSERT(!m_dmaBuf);
-    m_dmaBuf = DMABufBuffer::create(m_size, format, WTF::move(fds), WTF::move(offsets), WTF::move(strides), m_modifier);
+    m_dmaBuf = DMABufBuffer::create(WTF::move(*attributes));
     return true;
 }
 
@@ -256,66 +246,12 @@ EGLImage MemoryMappedGPUBuffer::createEGLImageFromDMABuf()
 {
     ASSERT(m_dmaBuf);
 
-    const auto& attributes = m_dmaBuf->attributes();
-    auto planeCount = attributes.fds.size();
-
-    Vector<EGLAttrib> eglAttributes {
-        EGL_WIDTH, static_cast<EGLAttrib>(m_allocatedSize.width()),
-        EGL_HEIGHT, static_cast<EGLAttrib>(m_allocatedSize.height()),
-        EGL_LINUX_DRM_FOURCC_EXT, static_cast<EGLAttrib>(attributes.fourcc)
-    };
-
-    static constexpr std::array planeAttributeNames = {
-        std::array { EGL_DMA_BUF_PLANE0_FD_EXT, EGL_DMA_BUF_PLANE0_OFFSET_EXT, EGL_DMA_BUF_PLANE0_PITCH_EXT, EGL_DMA_BUF_PLANE0_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE0_MODIFIER_LO_EXT },
-        std::array { EGL_DMA_BUF_PLANE1_FD_EXT, EGL_DMA_BUF_PLANE1_OFFSET_EXT, EGL_DMA_BUF_PLANE1_PITCH_EXT, EGL_DMA_BUF_PLANE1_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE1_MODIFIER_LO_EXT },
-        std::array { EGL_DMA_BUF_PLANE2_FD_EXT, EGL_DMA_BUF_PLANE2_OFFSET_EXT, EGL_DMA_BUF_PLANE2_PITCH_EXT, EGL_DMA_BUF_PLANE2_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE2_MODIFIER_LO_EXT },
-        std::array { EGL_DMA_BUF_PLANE3_FD_EXT, EGL_DMA_BUF_PLANE3_OFFSET_EXT, EGL_DMA_BUF_PLANE3_PITCH_EXT, EGL_DMA_BUF_PLANE3_MODIFIER_HI_EXT, EGL_DMA_BUF_PLANE3_MODIFIER_LO_EXT },
-    };
-
-    for (size_t i = 0; i < planeCount; ++i) {
-        const auto& names = planeAttributeNames[i];
-        std::array<EGLAttrib, 6> planeAttrs {
-            names[0], static_cast<EGLAttrib>(attributes.fds[i].value()),
-            names[1], static_cast<EGLAttrib>(attributes.offsets[i]),
-            names[2], static_cast<EGLAttrib>(attributes.strides[i])
-        };
-        eglAttributes.append(std::span<const EGLAttrib> { planeAttrs });
-
-        if (m_modifier != DRM_FORMAT_MOD_INVALID) {
-            std::array<EGLAttrib, 4> modifierAttrs {
-                names[3], static_cast<EGLAttrib>(m_modifier >> 32),
-                names[4], static_cast<EGLAttrib>(m_modifier & 0xffffffff)
-            };
-            eglAttributes.append(std::span<const EGLAttrib> { modifierAttrs });
-        }
-    }
-
-    eglAttributes.append(EGL_NONE);
-
     auto& display = WebCore::PlatformDisplay::sharedDisplay();
-    auto eglImage = display.createEGLImage(EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, nullptr, eglAttributes);
+    auto eglImage = m_dmaBuf->createEGLImage(display.glDisplay());
     if (!eglImage)
         LOG_ERROR("MemoryMappedGPUBuffer::createEGLImageFromDMABuf(), failed to export GBM buffer as EGLImage");
 
     return eglImage;
-}
-
-UnixFileDescriptor MemoryMappedGPUBuffer::exportGBMBufferObjectAsDMABuf(struct gbm_bo* bo, unsigned planeIndex)
-{
-    auto handle = gbm_bo_get_handle_for_plane(bo, planeIndex);
-    if (handle.s32 == -1) {
-        LOG_ERROR("MemoryMappedGPUBuffer::exportGBMBufferObjectAsDMABuf(), failed to obtain gbm handle for plane %u", planeIndex);
-        return { };
-    }
-
-    int fd = 0;
-    int ret = drmPrimeHandleToFD(gbm_device_get_fd(gbm_bo_get_device(bo)), handle.u32, DRM_CLOEXEC | DRM_RDWR, &fd);
-    if (ret < 0) {
-        LOG_ERROR("MemoryMappedGPUBuffer::exportGBMBufferObjectAsDMABuf(), failed to export dma-buf for plane %u", planeIndex);
-        return { };
-    }
-
-    return UnixFileDescriptor { fd, UnixFileDescriptor::Adopt };
 }
 
 void MemoryMappedGPUBuffer::updateContents(AccessScope& scope, const void* srcData, const IntRect& targetRect, unsigned bytesPerLine)

commit b6d89ee119656491a34cec4e0071bb51605c0bf8

[WPE] Android build broken after 4ca156ae@main https://bugs.webkit.org/show_bug.cgi?id=312373
Reviewed by Adrian Perez de Castro.

The refactoring of DMABuf code incorrectly exposed it to the android
build. We should properly guard the code that makes use of it.

No new tests as this is mainly a build fix.

* Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AcceleratedSurface.h:

Canonical link: https://commits.webkit.org/311284@main

Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AcceleratedSurface.h
@@ -268,9 +268,10 @@ private:
     class RenderTargetEGLImage final : public RenderTargetShareableBuffer {
     public:
         static std::unique_ptr<RenderTarget> create(AcceleratedSurface&, const WebCore::IntSize&, const BufferFormat&);
-        RenderTargetEGLImage(AcceleratedSurface&, const WebCore::IntSize&, EGLImage, WebCore::DMABufBufferAttributes&&, RendererBufferFormat::Usage);
 #if OS(ANDROID)
         RenderTargetEGLImage(AcceleratedSurface&, const WebCore::IntSize&, EGLImage, RefPtr<AHardwareBuffer>&&);
+#else
+        RenderTargetEGLImage(AcceleratedSurface&, const WebCore::IntSize&, EGLImage, WebCore::DMABufBufferAttributes&&, RendererBufferFormat::Usage);
 #endif
         ~RenderTargetEGLImage();
 