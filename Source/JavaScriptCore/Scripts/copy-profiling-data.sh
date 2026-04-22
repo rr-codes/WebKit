550e10f0c386b944b87b717567bb5301030f2848
Commit Queue <commit-queue@webkit.org>
2026-04-21 18:00:35 -0700

    Unreviewed, reverting 311713@main. https://bugs.webkit.org/show_bug.cgi?id=312943
    Broke iOS builds
    
    Reverted changeset:
    
    "[Xcode] Use different PGO profiles for each architecture"
    
    https://bugs.webkit.org/show_bug.cgi?id=312619
    https://commits.webkit.org/311713@main
    Canonical link: https://commits.webkit.org/311733@main

--- Source/JavaScriptCore/Scripts/copy-profiling-data.sh
+++ Source/JavaScriptCore/Scripts/copy-profiling-data.sh
@@ -1,34 +1,21 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 # Decompresses and copies PGO profiles from WebKitAdditions to a derived folder.
 
-PROFILES_FOLDER="${SCRIPT_INPUT_FILE_0}"
-EMPTY_PROFDATA="${SCRIPT_INPUT_FILE_1}"
-
-function decompress {
-    eval $(stat -s "${1}")
+if [ "${WK_ENABLE_PGO_USE}" = YES ]; then
+    eval $(stat -s "${SCRIPT_INPUT_FILE_0}")
     if [ ${st_size} -lt 1024 ]; then
         if [ "${CONFIGURATION}" = Production ]; then
-            echo "error: ${1} is <1KB, is it a Git LFS stub?"\
+            echo "error: ${SCRIPT_INPUT_FILE_0} is <1KB, is it a Git LFS stub?"\
                 "Ensure this file was checked out on a machine with git-lfs installed."
             exit 1
         else
-            echo "warning: ${1} is <1KB, is it a Git LFS stub?"\
+            echo "warning: ${SCRIPT_INPUT_FILE_0} is <1KB, is it a Git LFS stub?"\
                 "To build with production optimizations, ensure this file was checked out on a machine with git-lfs installed."\
                 "Falling back to stub profile data."
-            cp -vf "${EMPTY_PROFDATA}" "${2}"
+            cp -vf "${SCRIPT_INPUT_FILE_1}" "${SCRIPT_OUTPUT_FILE_0}"
         fi
     else
-        compression_tool -v -decode -i "${1}" -o "${2}"
+        compression_tool -v -decode -i "${SCRIPT_INPUT_FILE_0}" -o "${SCRIPT_OUTPUT_FILE_0}"
     fi
-}
-
-if [ "${WK_ENABLE_PGO_USE}" = YES ]; then
-    for arch in ${ARCHS_BASE:-${ARCHS}}; do
-        name=${arch}-${LLVM_TARGET_TRIPLE_VENDOR}-${LLVM_TARGET_TRIPLE_OS_VERSION}${LLVM_TARGET_TRIPLE_SUFFIX}.profile.compressed
-        decompress "${PROFILES_FOLDER}/${name}" "${SCRIPT_OUTPUT_FILE_0}"
-    done
-    echo "dependencies: ${SCRIPT_INPUT_FILE_0}/JavaScriptCore.profdata.compressed" > "${TARGET_TEMP_DIR}/copy-profiling-data.d"
-else
-    echo "dependencies: " > "${DERIVED_FILES_DIR}/copy-profiling-data.d"
 fi
