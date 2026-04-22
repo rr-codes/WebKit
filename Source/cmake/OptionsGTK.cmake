rr-codes/WebKit

commit af7a25e9646a6d5742b0848371fa0d412ab25ec0

[JHBuild] Bump libdrm version to '2.4.114' https://bugs.webkit.org/show_bug.cgi?id=298533
Reviewed by Carlos Alberto Lopez Perez.

Changeset 299155@main introduced the use of two new libdrm functions:
'drmModeCreateDumbBuffer' and 'drmModeDestroyDumbBuffer'. These functions
were introduced in 'libdrm' 2.4.114.

The change broke the build of WPEWebKit on systems featuring lower
version of 'libdrm'. For instance, Ubuntu 22.04.

This change upgrades 'libdrm' in JHBuild minimal dependencies. Distros
not featuring the 'libsoup' 2.4.114, should build with this moduleset
enabled.

* Source/cmake/OptionsGTK.cmake:
* Source/cmake/OptionsWPE.cmake:
* Tools/jhbuild/jhbuild-minimal.modules:

Canonical link: https://commits.webkit.org/299705@main

Source/cmake/OptionsGTK.cmake
@@ -322,6 +322,8 @@ if (USE_LIBDRM)
     set(CMAKE_REQUIRED_LIBRARIES LibDRM::LibDRM)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_VENDOR drmGetFormatModifierVendor xf86drm.h)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmGetFormatModifierName xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeCreateDumbBuffer xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeDestroyDumbBuffer xf86drm.h)
     unset(CMAKE_REQUIRED_LIBRARIES)
 endif ()
 

Source/cmake/OptionsWPE.cmake
@@ -411,6 +411,8 @@ if (USE_LIBDRM)
     set(CMAKE_REQUIRED_LIBRARIES LibDRM::LibDRM)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_VENDOR drmGetFormatModifierVendor xf86drm.h)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmGetFormatModifierName xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeCreateDumbBuffer xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeDestroyDumbBuffer xf86drm.h)
     unset(CMAKE_REQUIRED_LIBRARIES)
 endif ()
 

Tools/jhbuild/jhbuild-minimal.modules
@@ -179,9 +179,9 @@
   <meson id="libdrm" mesonargs="-Dtegra=enabled -Dcairo-tests=disabled -Dman-pages=disabled -Dvalgrind=disabled -Dudev=false -Dtests=false">
     <pkg-config>libdrm.pc</pkg-config>
     <branch module="mesa/drm.git"
-            version="2.4.113"
-            tag="libdrm-2.4.113"
-            checkoutdir="libdrm-2.4.113"
+            version="2.4.114"
+            tag="libdrm-2.4.114"
+            checkoutdir="libdrm-2.4.114"
             repo="git.freedesktop.org" />
   </meson>
 

commit e7798aeceff51b83ee62be60a2ad700c86771bbc

REGRESSION(299705@main): Fix bogus cmake HAVE_DRM_GET_FORMAT_MODIFIER_NAME redefinitions https://bugs.webkit.org/show_bug.cgi?id=312974
Reviewed by Claudio Saavedra.

Fixes a copy/paste regression from 299705@main (commit af7a25e9646a).

The two new WEBKIT_CHECK_HAVE_FUNCTION() checks added for
drmModeCreateDumbBuffer and drmModeDestroyDumbBuffer were both passed
HAVE_DRM_GET_FORMAT_MODIFIER_NAME as the HAVE_* variable to define,
which overwrites the result of the preceding drmGetFormatModifierName
check. On systems whose libdrm lacks the dumb-buffer functions
(e.g. Ubuntu 22.04 without the JHBuild moduleset) this leads to build
failures.

Covered by Ubuntu 22.04 release builders.

* Source/cmake/OptionsGTK.cmake:
* Source/cmake/OptionsWPE.cmake:

Canonical link: https://commits.webkit.org/311755@main

Source/cmake/OptionsGTK.cmake
@@ -296,8 +296,8 @@ if (USE_LIBDRM)
     set(CMAKE_REQUIRED_LIBRARIES LibDRM::LibDRM)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_VENDOR drmGetFormatModifierVendor xf86drm.h)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmGetFormatModifierName xf86drm.h)
-    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeCreateDumbBuffer xf86drm.h)
-    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeDestroyDumbBuffer xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_MODE_CREATE_DUMB_BUFFER drmModeCreateDumbBuffer xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_MODE_DESTROY_DUMB_BUFFER drmModeDestroyDumbBuffer xf86drm.h)
     unset(CMAKE_REQUIRED_LIBRARIES)
 endif ()
 

Source/cmake/OptionsWPE.cmake
@@ -427,8 +427,8 @@ if (USE_LIBDRM)
     set(CMAKE_REQUIRED_LIBRARIES LibDRM::LibDRM)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_VENDOR drmGetFormatModifierVendor xf86drm.h)
     WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmGetFormatModifierName xf86drm.h)
-    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeCreateDumbBuffer xf86drm.h)
-    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_GET_FORMAT_MODIFIER_NAME drmModeDestroyDumbBuffer xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_MODE_CREATE_DUMB_BUFFER drmModeCreateDumbBuffer xf86drm.h)
+    WEBKIT_CHECK_HAVE_FUNCTION(HAVE_DRM_MODE_DESTROY_DUMB_BUFFER drmModeDestroyDumbBuffer xf86drm.h)
     unset(CMAKE_REQUIRED_LIBRARIES)
 endif ()
 